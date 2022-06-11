f1f69812f48e8bcd501aae0283976c186252a3b4
Jitendra Meena <jitendrameena9575@gmail.com>
2022-06-11 17:10:13 +0530

    Email Authentications

--- RestFramework/RestFramework/settings.py
+++ RestFramework/RestFramework/settings.py
@@ -189,3 +189,10 @@ CACHES = {
 AUTH_USER_MODEL = 'accounts.User'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'computerprograming7999@gmail.com'
+EMAIL_HOST_PASSWORD = 'Meena@1234'
+DEFAULT_FROM_EMAIL = 'computerprograming7999@gmail.com'
+EMAIL_PORT = 587

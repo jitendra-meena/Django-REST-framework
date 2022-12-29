cf97f857d96cca32113a64319a3e48db3baec75d
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2022-12-29 23:05:20 +0530

    Update serializers.py

--- RestFramework/APIVIEW/serializers.py
+++ RestFramework/APIVIEW/serializers.py
@@ -11,10 +11,6 @@ class ProjectListSerializer(serializers.ModelSerializer):
         model = ProjectManager
         fields = '__all__'
         
-
-
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
